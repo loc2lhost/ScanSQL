543abc78e63ada2b5add4a1ee9509b10eaa7db4d
loc2lhost <48715681+loc2lhost@users.noreply.github.com>
2019-03-20 05:01:51 +0300

    Add files via upload

---
+++ SQL.py
@@ -0,0 +1,115 @@
+# !/usr/bin/python
+# coding=utf-8
+import requests, glob, time, random
+from urlparse import urlparse
+import os, sys
+from multiprocessing import Pool
+import codecs
+
+r = '\033[31m'
+g = '\033[32m'
+y = '\033[33m'
+b = '\033[34m'
+m = '\033[35m'
+c = '\033[36m'
+w = '\033[37m'
+rr = '\033[39m'
+
+
+def cls():
+    linux = 'clear'
+    windows = 'cls'
+    os.system([linux, windows][os.name == 'nt'])
+
+
+def print_logo():
+    clear = "\x1b[0m"
+    colors = [36, 32, 34, 35, 31, 37]
+
+    x = """
+
+
+
+          MMMMMMdhddmNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNmmdhdMMMMMM
+MMMMMMN+::----::/+oosyhhhdddmmmNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNmmmmddhhhysso+/::----::+NMMMMMM
+MMMMMMMMNNmy++++/////::::::::---------::/mMMMMmy+++/dMMMMMm/:::--------:::::::::////++++ymNNMMMMMMMM
+MMMMMMMMMMMNs/:::::-------:::////++++++``:NMMN//..-`:MMMMN-``++++++////::::------:::::/sNMMMMMMMMMMM
+MMMMMMMMMMMMMMMNh:::////++////:::::---::.`:NMMNh..:``yMMN:`.:::--:::::////+++///::/hNMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMdsso/::--::::::::------```/NNh.`ym``.mN/```-------:::::::--::/oosdMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMd/------::::://////+:``--``/mdo``.-``:+///////::::------/dMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMNdddhhyyss//::::::---````.m/:N-````----::::::/ssyyhhhddNMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy:-::::::::....yh``sh....::::::::::yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmmmmmmmds++++s.``.s++++ohmmmmmmmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmy/.````````.:smMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm/``````/mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN.``--``.NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+.+hddh+.+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNyNMN--NMNyNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMddMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+- Coded by Sudani-s3curity :)     
+
+ Note! : We don't Accept any responsibility for any illegal usage.       
+"""
+    for N, line in enumerate(x.split("\n")):
+        sys.stdout.write("\x1b[1;%dm%s%s\n" % (random.choice(colors), line, clear))
+        time.sleep(0.05)
+
+
+try:
+    with codecs.open('results/MyBeSqliVulnList.txt', mode='r', encoding='ascii', errors='ignore') as f:
+        ooo = f.read().splitlines()
+except IOError:
+    cls()
+    print_logo()
+    print r + '---------------------------------------------------------'
+    print r + '    [' + y + '-' + r + '] ' + c + ' file MyBeSqliVulnList.txt Not Found! frist Run GetSqliTargets.py!!!'
+    sys.exit()
+
+ooo = list((ooo))
+
+
+def get_url(url):
+    try:
+        error = ["DB Error", "SQL syntax;", "mysql_fetch_assoc", "mysql_fetch_array", "mysql_num_rows",
+                 "is_writable",
+                 "mysql_result", "pg_exec", "mysql_result", "mysql_num_rows", "mysql_query", "pg_query",
+                 "System Error",
+                 "io_error", "privilege_not_granted", "getimagesize", "preg_match", "mysqli_result", 'mysqli']
+
+        if url.startswith("http://"):
+            url = url.replace("http://", "")
+        elif url.startswith("https://"):
+            url = url.replace("https://", "")
+        else:
+            pass
+        for s in error:
+            Checksqli = requests.get('http://' + url + "'", timeout=5)
+
+            if s in Checksqli.text.encode('utf-8'):
+                SQLI = url.replace("'", "")
+                break
+        with open('results/SQLI_OK.txt', 'a') as xx:
+            xx.write(SQLI + '\n')
+        print c + '       [' + y + '+' + c + '] ' + g + url + g + ' [OK]'
+    except:
+        pass
+
+
+def goo():
+    try:
+        pp = Pool(65)
+        pp.map(get_url, ooo)
+    except:
+        pass
+
+
+if __name__ == '__main__':
+    cls()
+    print_logo()
+    print c + '       [' + y + '+' + c + '] ' + y + ' Scanning #'
+    goo():
+def main ("SQL http")
+    print("mysql_result")
+
+
+
